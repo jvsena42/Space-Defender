b9b7811177b63c1ec7c360f4552766b0a2e12a01
João Victor Sena <jvsena16@gmail.com>
2021-04-03 14:34:26 -0300

    [NEX]
    Destroy lasers

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private void OnBecameInvisible() {
+ 
+        Destroy(gameObject);
+ 
+    }
+}
