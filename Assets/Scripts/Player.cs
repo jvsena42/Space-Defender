466a11a435fe4cb89f3fcd2e16030fddc6b3c31d
João Victor Sena <jvsena16@gmail.com>
2021-04-25 09:49:41 -0300

    [NEW]
    Display player health

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -83,6 +83,10 @@ public class Player : MonoBehaviour
         FindObjectOfType<Level>().LoadGameOver();
     }
 
+    public int GetHealth(){
+        return health;
+    }
+
     IEnumerator FireContinuously(){
         while(true){
             GameObject laser = Instantiate(laserPrefab,transform.position,Quaternion.identity) as GameObject;
